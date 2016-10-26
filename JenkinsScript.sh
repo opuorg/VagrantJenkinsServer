a89f3cbc7122039bf0e1cccde343e5d411c68374
Opu Zaman <opu.zaman@gmail.com>
2016-10-26 14:58:23 -0400

    Added commands to add git in shell provision script, Increased memor of host machine.

--- JenkinsScript.sh
+++ JenkinsScript.sh
@@ -4,6 +4,11 @@ sudo apt-get install -y default-jre
 sudo apt-get install -y default-jdk
 java -version
 
+#Install Git
+sudo apt-get install -y git
+git config --global user.name "jenkins-user"
+git config --global user.email "jenkin-user@yahoo.com"
+
 #Install Jenkins
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
--- Vagrantfile
+++ Vagrantfile
@@ -9,5 +9,10 @@ Vagrant.configure("2") do |config|
   config.vm.network "forwarded_port", guest: 8080, host: 8070
 
   config.vm.provision "shell", path: "JenkinsScript.sh"
+  
+      config.vm.provider "virtualbox" do |v|
+        v.memory = 1024
+        v.cpus = 2
+    end
 
 end
